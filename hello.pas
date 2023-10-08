499def249642d4c4334d245af4b69e1242c27cd5
bluewater17 <fff.shev1705@gmail.com>
2023-10-08 20:11:12 +0300

    Create hello.pas

---
+++ hello.pas
@@ -0,0 +1,12 @@
+﻿begin
+  Println($'What language do you speak?  {newline}1 - eng {newline}2 - рус {newline}3 - 中国人');
+  var x := ReadInteger('Choose a number:');
+  if x = 1 then
+    Println('Hello')
+  else if x = 2 then
+    Println('привет')
+  else if x = 3 then
+    Println('屁股')
+  else
+    Println('there is no such language yet')
+end.
